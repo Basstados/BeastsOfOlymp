37cc2a6e29b091f27d9fd6e0f8e836079359157d
David Speck <david@saduras.de>
2013-10-21 22:21:22 +0200

    Simple rectangle level generator build

---
+++ Assets/Editor/LevelGenerationEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(LevelGeneration))]
+public class LevelGenerationEditor : Editor {
+	
+	private LevelGeneration myTarget;
+	private float progress = 0.0f;
+	
+	void Awake() {
+		// Get target
+		myTarget = (LevelGeneration) target;
+	}
+	
+	public override void OnInspectorGUI() {
+		// Draw default instpector as usually for now
+		DrawDefaultInspector();
+		
+		GUILayout.Space( 10 );
+		
+		// Add an button to start level generation
+		if(GUILayout.Button("Generate!")) {
+			// Clear up old level
+			myTarget.Reset();
+			// Start level generation
+			myTarget.Generate();
+		};
+		
+		// Add an button to clear generated level
+		if(GUILayout.Button("Clear")) {
+			// Clear up current level
+			myTarget.Reset();
+		}
+	}
+}
---
+++ Assets/LevelEditor/Scripts/LevelGeneration.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LevelGeneration : MonoBehaviour {
+	
+	public GameObject blockPrefab;
+	public int columns = 2;
+	public int rows = 2;
+	
+	//public float progress = 1.0f;
+	
+	/**
+	 * Instatiate blocks and position them in a grid with given number of columns and rows.
+	 */ 
+	public void Generate() {
+		//progress = 0.0f;
+		DoGeneration();	
+	}
+	
+	public void DoGeneration() {
+		// Create columns of the grid
+		for(int i=0; i<columns; i++ ) {
+			// Create rows of the grid
+			for(int j=0; j<rows; j++ ) {
+				// Instatiate gameobject from prefab
+				GameObject handle = (GameObject) Instantiate(blockPrefab);	
+				// Give gameobject a decent name
+				handle.name = blockPrefab.name + "[" + i + "," + j + "]";
+				// Make gameobject a child of us
+				handle.transform.parent = transform;
+				// Move child to it's position on the grid
+				handle.transform.localPosition = new Vector3( i, 0, j );
+				
+				//progress = (i * j) / (float)((columns-1) * (rows-1));
+				
+			}
+		}	
+	}
+	
+	/**
+	 * Remove all children elements, to clear generated level
+	 */ 
+	public void Reset() {
+		for( int i=transform.childCount-1; i>=0; i-- ){
+			if( Application.isEditor ) {
+				GameObject.DestroyImmediate( transform.GetChild( i ).gameObject );	
+			} else {
+				GameObject.Destroy( transform.GetChild( i ).gameObject	);
+			}
+		}
+	}
+
+	/*public float Progress {
+		get {
+			return this.progress;
+		}
+	}*/
+}
