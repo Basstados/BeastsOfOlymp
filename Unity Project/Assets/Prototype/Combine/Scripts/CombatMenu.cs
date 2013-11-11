e55071d0f9ae2bb4c98a775c0b2b2197d000ced4
David Speck <david@saduras.de>
2013-11-11 18:01:42 +0100

    Move CombatMenu on click to flied

---
+++ Unity Project/Assets/Prototype/Combine/Scripts/CombatMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void MoveTo( Vector3 screenPos ) {
+		screenPos.x -= Screen.width/2;
+		screenPos.y -= Screen.height/2;
+		transform.localPosition = screenPos;	
+	}
+}
--- Unity Project/Assets/Prototype/Combine/Scripts/Map.cs
+++ Unity Project/Assets/Prototype/Combine/Scripts/Map.cs
@@ -3,23 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Map : MonoBehaviour {
-
+	
+	// parameters used to generate the map
 	public GameObject blockPrefab;
 	public int columns = 2;
 	public int rows = 2;
 	
+	
+	// data about everything that exists on the map
 	Quad[,] quadMatrix;
-	//List<Monster> monsterList = new List<Monster>();
+	Dictionary<Vector2, Monster> monsterList;
+	
+	
+	// ui elements
+	public CombatMenu combatMenu;
 	
 	void Start() {
 		quadMatrix = new Quad[columns,rows];
 		
+		// initialize quadmatrix by getting the Battlefield quads from the scene
 		for(int i=0; i<columns; i++ ) {
 			for(int j=0; j<rows; j++ ) {
 				BattlefieldQuad currQuadMono = GameObject.Find(blockPrefab.name + "[" + i + "," + j + "]").GetComponent<BattlefieldQuad>();
 				Vector2 pos = new Vector2( (int) currQuadMono.transform.position.x, (int) currQuadMono.transform.position.z );
 				int pen = currQuadMono.movementPenalty;
 				quadMatrix[i,j] = new Quad(pos, pen);
+			}
+		}
+		
+		// find all monsters and add them to the dictionary
+		monsterList = new Dictionary<Vector2, Monster>(); // start with a clear monster dictionary
+		GameObject[] monsterGOs = GameObject.FindGameObjectsWithTag("Monster");
+		foreach( GameObject go in monsterGOs ) {
+			// use map position as key
+			Vector2 pos = WorldToMapPosition( go.transform.position );
+			Monster mon = go.GetComponent<Monster>();
+			
+			// make sure the monster has a valid position
+			if( 0 <= pos.x && pos.x < columns 
+				&& 0<= pos.y && pos.y < rows ) {
+				// monster pos is on the map
+				
+				// move monster the map position to make sure we don't get any glitches
+				// because of flooring
+				go.transform.position = transform.InverseTransformPoint( new Vector3( pos.x, 0f, pos.y ) );
+				// finally add monster to dictionary, using postion as key
+				monsterList.Add(pos, mon);
 			}
 		}
 	}
@@ -66,6 +95,37 @@ public class Map : MonoBehaviour {
 		Quad currentQuad = quadMatrix[(int)pos.x,(int)pos.y];
 		// do something with the taped quad
 		Debug.Log( currentQuad );
+		
+		// translate map position into screen position
+		Vector3 worldPos = MapToWorldPosition( pos );
+		Debug.Log( worldPos );
+		Vector3 screenPos = Camera.main.WorldToScreenPoint( worldPos );
+		screenPos.z = 0f;
+		Debug.Log( screenPos );
+		combatMenu.SendMessage("MoveTo", screenPos );
+	}
+	
+	/**
+	 * Translate any 3D world position into a discrete 2D map position
+	 * by taking X- and Z-coordiante and floor them.
+	 */
+	public Vector2 WorldToMapPosition( Vector3 worldPos ) {
+		Vector3 temp = transform.TransformPoint( transform.position );
+		Vector2 mapPos = Vector2.zero;
+		mapPos.x = Mathf.Floor( temp.x );
+		mapPos.y = Mathf.Floor( temp.z );
+		
+		return mapPos;
+	}
+	
+	/**
+	 * Translate any 2D map postion into a 3D word position.
+	 */ 
+	public Vector3 MapToWorldPosition( Vector2 mapPos ) {
+		Vector3 temp = new Vector3( mapPos.x, 0f, mapPos.y );
+		Vector3 worldPos = transform.InverseTransformPoint( temp );
+		
+		return worldPos;
 	}
 
 	public Quad[,] QuadMatrix {
