637202a063fde38a2f8892bdb29fdb135b636f93
David Speck <david@saduras.de>
2013-12-15 19:44:57 +0100

    Implement CombatLog

--- Unity Project/Assets/TheRealBoB/Scripts/Model/CombatLog/CombatLog.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/Model/CombatLog/CombatLog.cs
@@ -1,6 +1,40 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 
 public class CombatLog 
 {
-	
+	List<ILogEntry> log = new List<ILogEntry>();
+
+	public void Init() 
+	{
+		EventProxyManager.RegisterForEvent(EventName.UnitMoved, HandleUnitMoved);
+		EventProxyManager.RegisterForEvent(EventName.UnitAttacked, HandleUnitAttacked);
+	}
+
+	#region eventhandler
+	void HandleUnitMoved (object sender, EventProxyArgs args)
+	{
+		log.Add(new LogEntryMove(args as UnitMovedEvent));
+	}
+
+	void HandleUnitAttacked (object sender, EventProxyArgs args)
+	{
+		log.Add(new LogEntryAttack(args as UnitAttackedEvent));
+	}
+	#endregion
+
+	public string GetLast()
+	{
+		return log[log.Count-1].ToString();
+	}
+
+	public override string ToString ()
+	{
+		string str = "";
+		foreach(ILogEntry entry in log) {
+			str += entry.ToString() + "\n";
+		}
+
+		return str;
+	}
 }
