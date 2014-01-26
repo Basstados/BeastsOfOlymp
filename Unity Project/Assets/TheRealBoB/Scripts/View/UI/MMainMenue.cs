17a40ef2787b1936e5eccf46fe8407492c21f543
Basstados <basstados@web.de>
2014-01-26 15:25:47 +0100

    Main Menue
    Main Menue Scene added

---
+++ Unity Project/Assets/TheRealBoB/Scripts/View/UI/MMainMenue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MMainMenue : MonoBehaviour {
+
+	public void OnStartGameClick() 
+	{
+		Application.LoadLevel ("testScene");
+
+	}
+
+	public void OnCreditsClick() 
+	{
+		//Application.LoadLevel ("Credits");
+		Debug.Log("Credits Button Clicked");
+		
+	}
+
+	public void OnExitGameClick() 
+	{
+		Application.Quit();
+		
+	}
+}
