ace7e0e42e6aeb7f24c7fd0d305494e7be82c7a6
Basstados <basstados@web.de>
2014-01-27 04:41:47 +0100

    Credits added
    Credits geadded und BG fürs Menü

--- Unity Project/Assets/TheRealBoB/Scripts/View/UI/BMainMenue.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/View/UI/BMainMenue.cs
@@ -1,24 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MMainMenue : MonoBehaviour {
+public class BMainMenue : MonoBehaviour {
+
+	public GameObject mainMenueCreditsPanel;
 
 	public void OnStartGameClick() 
 	{
 		Application.LoadLevel ("testScene");
-
 	}
 
 	public void OnCreditsClick() 
 	{
-		//Application.LoadLevel ("Credits");
-		Debug.Log("Credits Button Clicked");
-		
+		gameObject.active = false;
+		mainMenueCreditsPanel.gameObject.active = true;
 	}
 
 	public void OnExitGameClick() 
 	{
 		Application.Quit();
-		
 	}
 }
---
+++ Unity Project/Assets/TheRealBoB/Scripts/View/UI/BMainMenueCredits.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BMainMenueCredits : MonoBehaviour {
+
+	public GameObject mainMenuePanel;
+
+	public void OnBackButtonClick() 
+	{
+		mainMenuePanel.gameObject.active = true;
+		gameObject.active = false;
+	}
+
+
+}
