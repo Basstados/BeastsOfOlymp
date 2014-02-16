a66613720d2180f97b5b034478e5a62f309e73fa
David Speck <david@saduras.de>
2014-02-16 15:01:35 +0100

    Queue particleSystem clones to enable multiple effects

--- Unity Project/Assets/TheRealBoB/Scripts/View/Effects/BParticleManager.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/View/Effects/BParticleManager.cs
@@ -5,14 +5,23 @@ using System.Collections.Generic;
 public class BParticleManager : MonoBehaviour {
 	
 	public GameObject[] particles;
-	Dictionary<string, GameObject> particleSystems = new Dictionary<string, GameObject>();
+	public int maxParticleOfOneKinde;
+	Dictionary<string, Queue<GameObject>> particleSystems = new Dictionary<string, Queue<GameObject>>();
 
 	#region singelton
 	static BParticleManager instance;
 	void Awake() 
 	{
 		foreach (GameObject go in particles) {
-			particleSystems.Add(go.name, go);
+			Queue<GameObject> particleQueue = new Queue<GameObject>();
+			particleQueue.Enqueue(go);
+			// create copies of the particle game object and add them to an queue
+			for (int i = 0; i < maxParticleOfOneKinde-1; i++) {
+				GameObject handle = (GameObject) Instantiate(go);
+				handle.transform.parent = this.transform;
+				particleQueue.Enqueue(handle);
+			}
+			particleSystems.Add(go.name, particleQueue);
 		}
 		instance = this;
 	}
@@ -20,7 +29,10 @@ public class BParticleManager : MonoBehaviour {
 
 	public static void PlayEffect(string particleName, Vector3 pos)
 	{
-		instance.particleSystems[particleName].transform.position = pos;
-		instance.particleSystems[particleName].SetActive(true);
+		GameObject go = instance.particleSystems[particleName].Dequeue();
+		// place and activate particle game object
+		go.transform.position = pos;
+		go.SetActive(true);
+		instance.particleSystems[particleName].Enqueue(go);
 	}
 }
