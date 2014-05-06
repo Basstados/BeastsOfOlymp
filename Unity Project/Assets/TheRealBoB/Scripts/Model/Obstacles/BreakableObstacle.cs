85058f595309e862d6340ecb6fc5e3df68c40abf
Saduras <davidspeck@gmx.de>
2014-05-06 17:19:12 +0200

    Add prototype-version of breakable obstacles.

--- Unity Project/Assets/TheRealBoB/Scripts/Model/Obstacles/BreakableObstacle.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/Model/Obstacles/BreakableObstacle.cs
@@ -1,38 +1,40 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-public class BurningOilField : Topping
+class BreakableObstacle : Topping
 {
-    int damagePerTurn = 1;
-
     /// <summary>
     /// Determines whether this topping in blocking the field or not.
-    /// Oil field are passable
+    /// Breakable obstacles are not passable.
     /// </summary>
     /// <returns><c>true</c> if this topping is blocking; otherwise, <c>false</c>.</returns>
     public override bool IsBlocking()
     {
-        return false;
+        return true;
     }
 
     /// <summary>
     /// The effect that will happen, if a unit stands on the field with this topping.
-    /// Deal damage each turn to the unit on this burning oil.
+    /// A unit can't stand on this field, so nothing happens here.
     /// </summary>
     /// <param name="mapTile">The mapTile the obstacle and the unit stand on.</param>
     /// <param name="unit">The unit wich will effected by what ever will happen.</param>
     public override void OnStayEffect(MapTile mapTile, Unit unit)
     {
-        unit.LoseHealth(damagePerTurn);
+        // do nothing
     }
 
     /// <summary>
     /// The effect hat will happen, if this field will be hit by an attack.
+    /// Just remove this obstacle.
     /// </summary>
     /// <param name="mapTile">The mapTile the obstacle and the unit stand on.</param>
     /// <param name="attack">The attack wich triggerd this effect.</param>
     public override void OnAttackEffect(MapTile mapTile, Attack attack)
     {
-
+        // remove this obstacle
+        mapTile.topping = null;
     }
 }
-
--- Unity Project/Assets/TheRealBoB/Scripts/View/BMapTile.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/View/BMapTile.cs
@@ -4,162 +4,191 @@ using System.Collections.Generic;
 using System;
 
 [System.Serializable]
-public class BMapTile : MonoBehaviour {
+public class BMapTile : MonoBehaviour
+{
 
-	public string prefabPath;
-	public Material defaultMaterial;
-	public Material moveRangeMaterial;
-	public Material attackRangeMaterial;
-	public Material pathMaterial;
-	public Material attackAreaMaterial;
-	public Material clickableMaterial;
+    public string prefabPath;
+    public Material defaultMaterial;
+    public Material moveRangeMaterial;
+    public Material attackRangeMaterial;
+    public Material pathMaterial;
+    public Material attackAreaMaterial;
+    public Material clickableMaterial;
 
-	public MapTile mapTile;
-	public ToppingType topping; // the topping type wich will be instantiate on game start
-	public Topping lastTopping; // used to track change of topping
-	[HideInInspector]
-	public ColorState colorState; // the current visual state of this instance
+    public MapTile mapTile;
+    public ToppingType topping; // the topping type wich will be instantiate on game start
+    public Topping lastTopping; // used to track change of topping
+    [HideInInspector]
+    public ColorState colorState; // the current visual state of this instance
 
-	public bool InRange {
-		get {
-			return !(colorState == ColorState.DEFAULT);
-		}
-	}
+    public bool InRange
+    {
+        get
+        {
+            return !(colorState == ColorState.DEFAULT);
+        }
+    }
 
-	public bool Clickable { get; set; }
+    public bool Clickable { get; set; }
 
-	public enum ToppingType {
-		NONE,
-		SOLID_OBSTACLE,
-		OIL,
-		BURNING_OIL
-	}
+    public enum ToppingType
+    {
+        NONE,
+        SOLID_OBSTACLE,
+        BREAKABLE_OBSTACLE,
+        OIL,
+        BURNING_OIL
+    }
 
-	public enum ColorState {
-		MOVERANGE,
-		ATTACKRANGE,
-		DEFAULT,
-		PATH,
-		CLICKABLE,
-		ATTACKAREA
-	}
+    public enum ColorState
+    {
+        MOVERANGE,
+        ATTACKRANGE,
+        DEFAULT,
+        PATH,
+        CLICKABLE,
+        ATTACKAREA
+    }
 
-	public void ChangeColorState(BMapTile.ColorState colorState)
-	{
-		StopCoroutine("TweenRoutine");
-		this.colorState = colorState;
+    public void ChangeColorState(BMapTile.ColorState colorState)
+    {
+        StopCoroutine("TweenRoutine");
+        this.colorState = colorState;
 
-		switch(colorState) {
-		case ColorState.MOVERANGE:
-			renderer.sharedMaterial = moveRangeMaterial;
-			this.Clickable = true;
-			break;
-		case ColorState.ATTACKRANGE:
-			renderer.sharedMaterial = attackRangeMaterial;
-			this.Clickable = true;
-			break;
-		case ColorState.PATH:
-			renderer.sharedMaterial = pathMaterial;
-			StartCoroutine(TweenRoutine(pathMaterial, Color.yellow, Color.gray));
-			break;
-		case ColorState.CLICKABLE:
-			renderer.sharedMaterial = clickableMaterial;
-			this.Clickable = true;
-			break;
-		case ColorState.ATTACKAREA:
-			renderer.sharedMaterial = attackAreaMaterial;
-			break;
-		case ColorState.DEFAULT:
-		default:
-			renderer.sharedMaterial = defaultMaterial;
-			this.Clickable = false;
-			break;
-		}
-	}
+        switch (colorState)
+        {
+            case ColorState.MOVERANGE:
+                renderer.sharedMaterial = moveRangeMaterial;
+                this.Clickable = true;
+                break;
+            case ColorState.ATTACKRANGE:
+                renderer.sharedMaterial = attackRangeMaterial;
+                this.Clickable = true;
+                break;
+            case ColorState.PATH:
+                renderer.sharedMaterial = pathMaterial;
+                StartCoroutine(TweenRoutine(pathMaterial, Color.yellow, Color.gray));
+                break;
+            case ColorState.CLICKABLE:
+                renderer.sharedMaterial = clickableMaterial;
+                this.Clickable = true;
+                break;
+            case ColorState.ATTACKAREA:
+                renderer.sharedMaterial = attackAreaMaterial;
+                break;
+            case ColorState.DEFAULT:
+            default:
+                renderer.sharedMaterial = defaultMaterial;
+                this.Clickable = false;
+                break;
+        }
+    }
 
-	void Update() {
-		if(lastTopping != mapTile.topping) {
-			switch(mapTile.topping.GetType().ToString()) {
-			case "SolidObstacle":
-				topping = ToppingType.SOLID_OBSTACLE;
-				break;
-			case "OilField":
-				topping = ToppingType.OIL;
-				break;
-			case "BurningOilField":
-				topping = ToppingType.BURNING_OIL;
-				break;
-			default:
-				topping = ToppingType.NONE;
-				break;
-			}
-			UpdateTopping();
-		}
+    void Update()
+    {
+        if (lastTopping != mapTile.topping)
+        {
+            if (mapTile.topping == null)
+            {
+                topping = ToppingType.NONE;
+            }
+            else
+            {
+                switch (mapTile.topping.GetType().ToString())
+                {
+                    case "SolidObstacle":
+                        topping = ToppingType.SOLID_OBSTACLE;
+                        break;
+                    case "OilField":
+                        topping = ToppingType.OIL;
+                        break;
+                    case "BurningOilField":
+                        topping = ToppingType.BURNING_OIL;
+                        break;
+                    case "BreakableObstacle":
+                        topping = ToppingType.BREAKABLE_OBSTACLE;
+                        break;
+                    default:
+                        topping = ToppingType.NONE;
+                        break;
+                }
+            }
+            
+            UpdateTopping();
+        }
 
-	}
+    }
 
-	public void UpdateTopping()
-	{
-		switch(topping)
-		{
-		case ToppingType.NONE:
-			mapTile.topping = null;
-			break;
-		case ToppingType.SOLID_OBSTACLE:
-			mapTile.topping = new SolidObstacle();
-			break;
-		case ToppingType.OIL:
-			mapTile.topping = new OilField();
-			break;
-		case ToppingType.BURNING_OIL:
-			mapTile.topping = new BurningOilField();
-			break;
-		}
+    public void UpdateTopping()
+    {
+        switch (topping)
+        {
+            case ToppingType.NONE:
+                mapTile.topping = null;
+                break;
+            case ToppingType.SOLID_OBSTACLE:
+                mapTile.topping = new SolidObstacle();
+                break;
+            case ToppingType.OIL:
+                mapTile.topping = new OilField();
+                break;
+            case ToppingType.BURNING_OIL:
+                mapTile.topping = new BurningOilField();
+                break;
+            case ToppingType.BREAKABLE_OBSTACLE:
+                mapTile.topping = new BreakableObstacle();
+                break;
+        }
 
-		// TODO make this more elegant (not copy-code)
+        // TODO make this more elegant (not copy-code)
 
-		// if topping has changed, instantiate new prefab
-		//if(lastTopping != topping) {
-			// remove old children objects
-			List<GameObject> children = new List<GameObject>();
-			for (int i = 0; i < transform.childCount; i++) {
-				children.Add(transform.GetChild(i).gameObject);
-			}
-			foreach(GameObject go in children)
-				DestroyImmediate(go);
-			
-			// spawn prefab if there is any
-			if(topping != BMapTile.ToppingType.NONE) {
-				Debug.Log(prefabPath + topping.ToString());
-				GameObject prefab = Resources.Load<GameObject>(prefabPath + topping.ToString()); // note: not .prefab!
-				GameObject handle = (GameObject) Instantiate(prefab, Vector3.zero, prefab.transform.rotation);
-				handle.transform.parent = this.transform;
-				handle.transform.localPosition = Vector3.zero;
-				
-				
-			}
-			
-			lastTopping = mapTile.topping;
-		//}
-	}
+        // if topping has changed, instantiate new prefab
 
-	IEnumerator TweenRoutine(Material mat, Color c1, Color c2)
-	{
-		float t = 0;
-		bool rising = true;
+        // remove old children objects
+        List<GameObject> children = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children.Add(transform.GetChild(i).gameObject);
+        }
+        foreach (GameObject go in children)
+            DestroyImmediate(go);
 
-		while(true) {
-			if(rising) {
-				t += Time.deltaTime;
-				if(t>1) rising = false;
-			} else {
-				t -= Time.deltaTime;
-				if(t<0) rising = true;
-			}
+        // spawn prefab if there is any
+        if (topping != BMapTile.ToppingType.NONE)
+        {
+            Debug.Log(prefabPath + topping.ToString());
+            GameObject prefab = Resources.Load<GameObject>(prefabPath + topping.ToString()); // note: not .prefab!
+            GameObject handle = (GameObject)Instantiate(prefab, Vector3.zero, prefab.transform.rotation);
+            handle.transform.parent = this.transform;
+            handle.transform.localPosition = Vector3.zero;
 
-			mat.color = Color.Lerp(c1,c2,t);
-			// wait 1 frame
-			yield return 0;
-		}
-	}
+
+        }
+
+        lastTopping = mapTile.topping;
+    }
+
+    IEnumerator TweenRoutine(Material mat, Color c1, Color c2)
+    {
+        float t = 0;
+        bool rising = true;
+
+        while (true)
+        {
+            if (rising)
+            {
+                t += Time.deltaTime;
+                if (t > 1) rising = false;
+            }
+            else
+            {
+                t -= Time.deltaTime;
+                if (t < 0) rising = true;
+            }
+
+            mat.color = Color.Lerp(c1, c2, t);
+            // wait 1 frame
+            yield return 0;
+        }
+    }
 }
