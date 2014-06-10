0ee881444491f1776229d2d51a3f9c494a372b68
jschlosser <jan.schlosser@school4games.de>
2014-06-10 22:47:00 +0200

    camera Test, Insert new Logo, small Menue changes

--- Unity Project/Assets/TheRealBoB/Scripts/View/UI/BMainMenueSubPanel.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/View/UI/BMainMenueSubPanel.cs
@@ -5,10 +5,17 @@ using System.Collections;
 public class BMainMenueSubPanel : MonoBehaviour {
 
 	public GameObject mainMenuePanel;
+	public GameObject tutorialListPanel;
 
 	public void OnBackButtonClick() 
 	{
 		mainMenuePanel.gameObject.SetActive(true);
+		gameObject.SetActive(false);
+	}
+
+	public void OnBackToTutListButtonClick() 
+	{
+		tutorialListPanel.gameObject.SetActive(true);
 		gameObject.SetActive(false);
 	}
 
