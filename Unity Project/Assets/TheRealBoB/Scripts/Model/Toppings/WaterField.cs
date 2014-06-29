d80e56c2d87d546617281c0da0d1fb4d409e15ce
lkokemohr <lars@lkokemohr.de>
2014-06-29 09:11:24 +0200

    Bugfix: damage from standing in water while being attacked with lightning caused a crash

--- Unity Project/Assets/TheRealBoB/Scripts/Model/Toppings/WaterField.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/Model/Toppings/WaterField.cs
@@ -38,10 +38,11 @@ public class WaterField : Topping
 			// but in order to avoid unknown consequences I kept using the code pattern found in CAttackUnit:96
 			// when target died fire event AFTER burning was performed
 			if(mapTile.unit.HealthPoints <= 0) {
+				Unit victim = mapTile.unit;
 				// remove target from map
-				mapTile.unit.mapTile.unit = null;
+				mapTile.unit = null;
 				// fire event
-				EventProxyManager.FireEvent(this, new UnitDiedEvent(mapTile.unit));
+				EventProxyManager.FireEvent(this, new UnitDiedEvent(victim));
 			}
 
 			//this.Destroy();
