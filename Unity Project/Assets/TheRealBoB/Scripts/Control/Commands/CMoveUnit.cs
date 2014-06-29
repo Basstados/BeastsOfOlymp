a9ae32d31dab183fc72631c8a00cba0ec0d9d26c
jschlosser <jan.schlosser@school4games.de>
2014-06-29 06:56:33 +0200

    Level 4 Instantiate new & CMoveUnit Script change

--- Unity Project/Assets/TheRealBoB/Scripts/Control/Commands/CMoveUnit.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/Control/Commands/CMoveUnit.cs
@@ -8,7 +8,7 @@ public class CMoveUnit : ICommand
 	Controller controller;
 	Unit unit;
 	Path path;
-
+	
 	public CMoveUnit (Model model, Unit unit, Path path, Controller controller)
 	{
 		this.model = model;
@@ -16,16 +16,19 @@ public class CMoveUnit : ICommand
 		this.path = path;
 		this.controller = controller;
 	}
-
-
+	
+	
 	public void Execute()
 	{
-		// check if start and destination are the same
-		if(unit.mapTile != path.Last) {
-			// stop if target is to far away for unit move
-			if(path.Cost > unit.MovePoints)
-				return;
-
+		// stop if start and destination are the same
+		if((unit.mapTile == path.Last)
+		   // or if target is to far away for unit move
+		   || (path.Cost > unit.MovePoints))
+		{
+			path = new Path();
+		}
+		else
+		{
 			// we are now sure, that unit is allowed to move and target is in range
 			// now performce actual move
 			model.MoveUnit(unit, path.Last);
@@ -34,11 +37,11 @@ public class CMoveUnit : ICommand
 				if(path[i].topping != null)
 					path[i].topping.OnStayEffect(unit);
 			}
-
+			
 			// clear units move resource, since only one move per turn is permitted
 			unit.MovePoints = 0;
 		}
-
+		
 		// after movement fire event
 		EventProxyManager.FireEvent(this,new UnitMovedEvent(unit, path));
 	}
@@ -48,7 +51,7 @@ public class UnitMovedEvent : EventProxyArgs
 {
 	public Unit unit;
 	public Path path;
-
+	
 	public UnitMovedEvent (Unit unit, Path path)
 	{
 		this.name = EventName.UnitMoved;
