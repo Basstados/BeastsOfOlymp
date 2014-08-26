a50ca73b78b3782c7047a5778853a23f1fd81d83
jschlosser <jan.schlosser@school4games.de>
2014-08-26 15:46:47 +0200

    Playing Sound if Object is destroyed

--- Unity Project/Assets/TheRealBoB/Scripts/View/Map/BTopping.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/View/Map/BTopping.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class BTopping : MonoBehaviour {
 
+	//public GameObject SoundOilVase;
+	public AudioClip destroySound;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 
     public void DestroyTopping()
     {
+		audio.PlayOneShot(destroySound, 1F);
         // remove yourself from the game
-        Destroy(this.gameObject);
+		Destroy(this.gameObject, audio.clip.length);
+
     }
 }
