cdb8d63d4995615379da96cfb9b50c11ed23a382
jschlosser <jan.schlosser@school4games.de>
2014-08-26 18:07:14 +0200

    Cleanup Trash in BTopping Script

--- Unity Project/Assets/TheRealBoB/Scripts/View/Map/BTopping.cs
+++ Unity Project/Assets/TheRealBoB/Scripts/View/Map/BTopping.cs
@@ -2,20 +2,14 @@
 using System.Collections;
 
 public class BTopping : MonoBehaviour {
-
-	//public GameObject SoundOilVase;
+	
 	public AudioClip destroySound;
 	//public ParticleSystem myparticle;
 	public GameObject particleTest;
-	//public GameObject ownPosition;
 
-	//public objectPos = object.transform.position;
 	// Use this for initialization
 	void Start () {
-		//myparticle = GetComponent<ParticleSystem>();
-		//particleTest = GetComponent<ParticleSystem>();
-
-
+	
 	}
 
     public void DestroyTopping()
@@ -27,24 +21,8 @@ public class BTopping : MonoBehaviour {
 		particleTest.particleSystem.Play();
 		Destroy(particleTest.particleSystem.gameObject, 0.5F);
 		}
-		
 
-
-		/*
-		Vector3 position2 = new Vector3 (ownPosition.position);
-
-
-		Vector3 positionOwn = ownPosition.transform.position;
-		gameObject.particleSystem.Play();
-
-		particleSystem.transform.parent=null; // detach particle system
-
-		particleTest.transform.position = positionOwn;
-		particleTest.particleSystem.Play();
-
-		Destroy(particleSystem.gameObject, 3);
-        remove yourself from the game*/
-
+		/*remove yourself from the game*/
 		Destroy(this.gameObject, audio.clip.length);
 
     }
